e5d9b610bc20db29f681137dbcdfdb9686c7ea9e
kathiravan292 <kkathiravan785@gmail.com>
2025-07-08 16:43:43 +0530

    new files

---
+++ java.js
@@ -0,0 +1,17 @@
+// ////////////function///////
+// function expression////////
+let greet = function() {
+  console.log("Hello!");
+};
+
+greet(); 
+
+/////multiline arrow function/////
+let multipy =(a,b)=>{
+    let result=a*b;
+    return result;
+}
+console.log((2,3));
+///////one line function////
+let add = (a, b) => a + b;
+console.log(add(2, 3));
