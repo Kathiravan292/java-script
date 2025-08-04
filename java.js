4eaf49b13e489b9a75e9827a848fd8afe3790f8f
kathiravan C <124439980+Kathiravan292@users.noreply.github.com>
2025-08-05 00:04:31 +0530

    Update java.js

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ let multipy =(a,b)=>{
     let result=a*b;
     return result;
 }
-  console.log((2,3));
+ console.log((2,3));
 ///////one line function////
 let add = (a, b) => a + b;
 console.log(add(2, 3));
